650dadd541a2b4b0aa3985583b0da0c572d77066
Julggs <julio_gabrielira@hotmail.com>
2023-09-20 20:59:02 -0300

    input e js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,10 @@ let nome = document.querySelector("#nome")
 
 const campoQtde = document.querySelector("#qtde")
 const output = document.querySelector("output")
+const campoTipo = document.querySelector("#tipo")
 
 campoQtde.addEventListener("change", calcular)
+campoTipo.addEventListener("change", calcular)
 
 // Calcular o preço do orçcamento
 
@@ -12,6 +14,12 @@ function calcular(){
     // R$ 100 por página
     let qtde = campoQtde.value
     let valor = qtde * 100
+    
+    if (campoTipo.value == 2) valor = valor + 1000
+    
+
+    
+
     output.innerText = `R$ ${valor},00`
 
 }
